8c8b8d2aad5740611c9c016411799987c61fa150
Soromytko <NikitaSoromytko@gmail.com>
2025-06-24 00:54:28 +0300

    Fix for Linux build

--- internal/logging.h
+++ internal/logging.h
@@ -18,9 +18,9 @@ namespace rendell_text
 	logx::Logger* get_logger();
 }
 
-#define RT_CRITICAL(formatStr, ...) rendell_text::get_logger()->critical(std::format(formatStr, __VA_ARGS__))
-#define RT_ERROR(formatStr, ...) rendell_text::get_logger()->error(std::format(formatStr, __VA_ARGS__))
-#define RT_WARNING(format, ...) rendell_text::get_logger()->warning(std::format(formatStr, __VA_ARGS__))
-#define RT_INFO(formatStr, ...) rendell_text::get_logger()->info(std::format(formatStr, __VA_ARGS__))
-#define RT_DEBUG(formatStr, ...) rendell_text::get_logger()->debug(std::format(formatStr, __VA_ARGS__))
-#define RT_TRACE(formatStr, ...) rendell_text::get_logger()->trace(std::format(formatStr, __VA_ARGS__))
+#define RT_CRITICAL(formatStr, ...) rendell_text::get_logger()->critical(std::format(formatStr, ##__VA_ARGS__))
+#define RT_ERROR(formatStr, ...) rendell_text::get_logger()->error(std::format(formatStr, ##__VA_ARGS__))
+#define RT_WARNING(formatStr, ...) rendell_text::get_logger()->warning(std::format(formatStr, ##__VA_ARGS__))
+#define RT_INFO(formatStr, ...) rendell_text::get_logger()->info(std::format(formatStr, ##__VA_ARGS__))
+#define RT_DEBUG(formatStr, ...) rendell_text::get_logger()->debug(std::format(formatStr, ##__VA_ARGS__))
+#define RT_TRACE(formatStr, ...) rendell_text::get_logger()->trace(std::format(formatStr, ##__VA_ARGS__))
--- res/Shaders/TextRenderer.fs
+++ res/Shaders/TextRenderer.fs
@@ -10,7 +10,7 @@ uniform vec4 u_BackgroundColor;
 
 void main()
 {
-	const float sampled = texture2DArray(u_Textures, vec3(v_UV, v_TextureIndex)).r;
+	const float sampled = texture(u_Textures, vec3(v_UV, v_TextureIndex)).r;
 	const float sampledInverse = 1.0 - sampled;
 
 	const vec3 baseColor = u_TextColor.rgb * sampled + u_BackgroundColor.rgb * sampledInverse;
